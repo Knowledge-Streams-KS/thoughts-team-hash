b6d6e4a1eef63828b2bcb0909aebc3fa4af9b539
hamza2439100 <hamza24391@gmail.com>
2023-09-30 23:16:16 +0500

    User model is setup and abstract user is created also signup and sign in page frontend is done

--- Thoughts/settings.py
+++ Thoughts/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'posts',
+    'users',
+    'bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -116,8 +119,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+AUTH_USER_MODEL = 'users.User'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+import os
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
---
+++ posts/urls.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+from django.urls import path
+from .views import *
+from . import views
+
+
+urlpatterns = [
+    #  path('create-tweet/', views.create_tweet),
+    #  path('view-tweet/', views.view_tweet),
+    #  path('delete-tweet/', views.delete_tweet),
+    #  path('update-tweet/', views.update_tweet),
+    #  path('created/',views.createform_html),
+    #  path('shared_with/',views.shared_with),
+    #  path('comment/',views.comment_form),
+
+    #  path("listview/", TweetListView.as_view(), name="tweet-list"),
+    #  path("tweet/<pk>/", TweetDetailView.as_view(), name="tweet-detail"),
+    #  path("tweetcreate/", TweetCreateView.as_view(), name="tweet-create"),
+    #  path("about/", AboutView.as_view()),
+
+     # path('done/',views.createform_html),
+
+
+
+]
