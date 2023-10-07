b7398e172e58960e9e2e7c76e960e071b06ea1d9
hamza2439100 <hamza24391@gmail.com>
2023-10-08 04:41:00 +0500

    user delete and view functionality implemented but update

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from users.models import User
 # Create your models here.
+
+# class Post(models.Model):
+#     name = models.CharField(max_length=50)
+#     content = models.CharField(max_length=450)
+#     user = models.ForeignKey(User,on_delete= CASCADE)
--- users/admin.py
+++ users/admin.py
@@ -2,4 +2,13 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
-admin.site.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display=["id","username"]
+    exclude=["content"]
+    # fields =["name"]
+    # readonly_fields = ["id","created_at"]
+    search_fields=['username']
+
+
+
+admin.site.register(User,UserAdmin)
