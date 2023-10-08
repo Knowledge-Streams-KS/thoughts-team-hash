5e8d9fc81eec3e3429bb5dbb7014f2a19c905c1b
hamza2439100 <hamza24391@gmail.com>
2023-10-08 05:01:13 +0500

    models setted up

--- posts/models.py
+++ posts/models.py
@@ -2,7 +2,17 @@ from django.db import models
 from users.models import User
 # Create your models here.
 
-# class Post(models.Model):
-#     name = models.CharField(max_length=50)
-#     content = models.CharField(max_length=450)
-#     user = models.ForeignKey(User,on_delete= CASCADE)
+def image_file_handler(instance,filename):
+    return (f"image/{instance}/{filename}")
+
+
+
+class Post(models.Model):
+    user = models.ForeignKey(User,on_delete= models.CASCADE)
+    name = models.CharField(max_length=50)
+    content = models.CharField(max_length=450)
+    created_at = models.DateTimeField(auto_now_add=True)
+    media = models.FileField(upload_to= image_file_handler,blank= True)
+    shared_with = models.ManyToManyField(User,related_name= "shared")
+
+    
